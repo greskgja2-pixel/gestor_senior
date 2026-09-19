1ea36b83271451e558a5ef8d905ede114016184c
greskgja2-pixel <greskgja2@gmail.com>
2026-09-18 22:12:29 -0300

    feat: criar pagina Central de Protecao ROAS

---
+++ app/protecao-roas/page.js
@@ -0,0 +1,7 @@
+import ProtectionRoasDashboard from './ProtectionRoasDashboard';
+
+export const dynamic='force-dynamic';
+
+export default function ProtecaoRoasPage(){
+  return <ProtectionRoasDashboard/>;
+}
